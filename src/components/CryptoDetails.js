7c4d60bc759db28f0cabd9a20d042fc634dcc4fc
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2024-12-08 23:58:49 +0530

    Update CryptoDetails.js

--- src/components/CryptoDetails.js
+++ src/components/CryptoDetails.js
@@ -12,7 +12,9 @@ const HighLowIndicator = ({ currentPrice, high, low }) => {
   2
   11
   1
-  1
+  1let { coinId } = useParams();
+  let navigate = useNavigate();
+  let { getCoinData, coinData: data, currency } = useContext(CryptoContext);
   1
   11
   1
