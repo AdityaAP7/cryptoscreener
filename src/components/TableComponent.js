306bd095cc8b9758b6682560aee2fb2a6531684b
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2024-12-21 23:44:10 +0530

    Update TableComponent.js

--- src/components/TableComponent.js
+++ src/components/TableComponent.js
@@ -1,4 +1,4 @@
-qqqqqqqqqqqqewqimport React, { useContext } from "react";
+qqqqqqqqqqqqqewqimport React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { CryptoContext } from "./../context/CryptoContext";
 import Pagination from "./Pagination";
