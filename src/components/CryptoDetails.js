7715397e486afba0953657640ff36dc994ba5f92
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2024-12-27 23:54:11 +0530

    Update CryptoDetails.js

--- src/components/CryptoDetails.js
+++ src/components/CryptoDetails.js
@@ -1,4 +1,4 @@
-aa/* eslint-disable react-hooks/exhaustive-deps */
+aaa/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useContext, useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import { useNavigate, useParams } from "react-router-dom";
