89ae999dbe01df66e9c6029898b6f2cfc8eaab29
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2025-01-08 23:25:33 +0530

    Update Filters.js

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -50,7 +50,7 @@ const Filters = () => {
           
             
           
-            placeholder="usd"
+
             className="w-16 rounded bg-gray-200 placeholder:text-gray-100
      pl-2 required outline-0 border border-transparent 
      focus:border-[#ffc757] leading-4
